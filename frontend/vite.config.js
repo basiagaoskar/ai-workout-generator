45c70aa80c067dab5595efc30907b12f6a79f38a
Oskar Basiaga <basiagaoskar@gmail.com>
2025-10-14 00:51:35 +0200

    feat(styling): add DaisyUI and Tailwind

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,9 +10,10 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    tailwindcss(),
   ],
-   server: {
-    host: '0.0.0.0', 
+  server: {
+    host: '0.0.0.0',
     port: 5173,
     watch: {
       usePolling: true,
