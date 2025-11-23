269577943831108c187706adf6f7f8dbe737ebff
Oskar Basiaga <basiagaoskar@gmail.com>
2025-11-23 13:32:08 +0100

    style: enhance authentication UI

--- frontend/src/pages/Auth/LoginForm.jsx
+++ frontend/src/pages/Auth/LoginForm.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { KeyRound, Mail, Loader2 } from "lucide-react";
 import { GoogleLogin } from "@react-oauth/google";
+
 import { useAuthStore } from "../../store/useAuthStore";
 
 function LoginForm() {
@@ -71,23 +72,31 @@ function LoginForm() {
 				</button>
 
 				<div className="divider opacity-75">Or login with</div>
-				<div className="flex flex-col lg:flex-row w-full gap-8 justify-center">
+				<div className="flex flex-col lg:flex-row w-full gap-4 justify-center">
 					<div className="w-full lg:w-1/2 flex justify-center">
-						<GoogleLogin
-							onSuccess={(credentialResponse) => {
-								authWithGoogle(credentialResponse.credential);
-							}}
-						/>
+						<div className="w-full">
+							<GoogleLogin
+								onSuccess={(credentialResponse) => {
+									authWithGoogle(credentialResponse.credential);
+								}}
+								type="standard"
+								theme="outline"
+								size="large"
+								text="signin_with"
+								shape="rectangular"
+								width="100%"
+							/>
+						</div>
 					</div>
 
-					<button className="btn w-full lg:w-1/2 bg-black text-white border-black">
+					<button type="button" className="btn w-full lg:w-1/2 bg-black text-white border-black hover:bg-gray-800 hover:border-gray-800">
 						<svg aria-label="Apple logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1195 1195">
 							<path
 								fill="white"
 								d="M1006.933 812.8c-32 153.6-115.2 211.2-147.2 249.6-32 25.6-121.6 25.6-153.6 6.4-38.4-25.6-134.4-25.6-166.4 0-44.8 32-115.2 19.2-128 12.8-256-179.2-352-716.8 12.8-774.4 64-12.8 134.4 32 134.4 32 51.2 25.6 70.4 12.8 115.2-6.4 96-44.8 243.2-44.8 313.6 76.8-147.2 96-153.6 294.4 19.2 403.2zM802.133 64c12.8 70.4-64 224-204.8 230.4-12.8-38.4 32-217.6 204.8-230.4z"
 							></path>
 						</svg>
-						Apple
+						Login with Apple
 					</button>
 				</div>
 			</form>
